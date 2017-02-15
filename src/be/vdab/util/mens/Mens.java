a083381f4881dfb539e0ed853ecc157ee855faf2
Administrator <Administrator@MilkyWay>
2017-02-15 10:48:33 +0100

    Mens done

--- src/be/vdab/util/mens/Mens.java
+++ src/be/vdab/util/mens/Mens.java
@@ -18,29 +18,29 @@ import java.util.Objects;
 public class Mens implements Comparable<Mens>{
    
     private String naam;
-    private String [] rijbewijs;
+    private Rijbewijs [] rijbewijs;
 
     public Mens(String naam) {
         this.naam = naam;
-        rijbewijs = new String [0];
+        rijbewijs = new Rijbewijs [0];
     }
     
     
 
     public Mens(String naam, Rijbewijs r) {
         this.naam = naam;
-        rijbewijs = new String [1];
-        this.rijbewijs[0] = (r.toString());        
+        rijbewijs = new Rijbewijs [1];
+        this.rijbewijs[0] = r;        
         
     }
 
     public Mens(String naam, Rijbewijs r, Rijbewijs s) {
         int i = 0;
         this.naam = naam;
-        String[] rijbewijstemp = new String [2];
-        rijbewijstemp[0] = (r.toString());
+        Rijbewijs[] rijbewijstemp = new Rijbewijs [2];
+        rijbewijstemp[0] = r;
         i++;
-        if(!r.equals(s)){ rijbewijstemp[i] = (s.toString()); i++;}
+        if(!r.equals(s)){ rijbewijstemp[i] = s; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
         
@@ -49,11 +49,11 @@ public class Mens implements Comparable<Mens>{
     public Mens(String naam, Rijbewijs r, Rijbewijs s, Rijbewijs t) {
         int i = 0;
         this.naam = naam;
-        String[] rijbewijstemp = new String [3];
-        rijbewijstemp[0] = (r.toString());
+        Rijbewijs[] rijbewijstemp = new Rijbewijs [3];
+        rijbewijstemp[0] = r;
         i++;
-        if(!s.equals(r)){ rijbewijstemp[i] = (s.toString()); i++;}
-        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = (t.toString()); i++;}
+        if(!s.equals(r)){ rijbewijstemp[i] = s; i++;}
+        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = t; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
     }
@@ -61,12 +61,12 @@ public class Mens implements Comparable<Mens>{
     public Mens(String naam, Rijbewijs r, Rijbewijs s, Rijbewijs t, Rijbewijs u) {
         int i = 0;
         this.naam = naam;
-        String[] rijbewijstemp = new String [4];
-         rijbewijstemp[0] = (r.toString());
+        Rijbewijs[] rijbewijstemp = new Rijbewijs [4];
+        rijbewijstemp[0] = r;
         i++;
-        if(!s.equals(r)){ rijbewijstemp[i] = (s.toString()); i++;}
-        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = (t.toString()); i++;}
-        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = (u.toString()); i++;}
+        if(!s.equals(r)){ rijbewijstemp[i] = s; i++;}
+        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = t; i++;}
+        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = u; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
         
@@ -74,13 +74,13 @@ public class Mens implements Comparable<Mens>{
     public Mens(String naam, Rijbewijs r, Rijbewijs s, Rijbewijs t, Rijbewijs u, Rijbewijs v) {
         int i = 0;
         this.naam = naam;
-        String[] rijbewijstemp = new String [5];
-         rijbewijstemp[0] = (r.toString());
+        Rijbewijs[] rijbewijstemp = new Rijbewijs [5];
+        rijbewijstemp[0] = r;
         i++;
-        if(!s.equals(r)){ rijbewijstemp[i] = (s.toString()); i++;}
-        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = (t.toString()); i++;}
-        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = (u.toString()); i++;}
-        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = (v.toString()); i++;}
+        if(!s.equals(r)){ rijbewijstemp[i] = s; i++;}
+        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = t; i++;}
+        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = u; i++;}
+        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = v; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
     }
@@ -88,14 +88,14 @@ public class Mens implements Comparable<Mens>{
     public Mens(String naam, Rijbewijs r, Rijbewijs s, Rijbewijs t, Rijbewijs u, Rijbewijs v, Rijbewijs w) {
         int i = 0;
         this.naam = naam;
-        String[] rijbewijstemp = new String [6];
-         rijbewijstemp[0] = (r.toString());
+        Rijbewijs[] rijbewijstemp = new Rijbewijs [6];
+        rijbewijstemp[0] = r;
         i++;
-        if(!s.equals(r)){ rijbewijstemp[i] = (s.toString()); i++;}
-        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = (t.toString()); i++;}
-        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = (u.toString()); i++;}
-        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = (v.toString()); i++;}
-        if(!w.equals(r) && !w.equals(s) && !w.equals(t) && !w.equals(u) && !w.equals(v)){ rijbewijstemp[i] = (w.toString()); i++;}
+        if(!s.equals(r)){ rijbewijstemp[i] = s; i++;}
+        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = t; i++;}
+        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = u; i++;}
+        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = v; i++;}
+        if(!w.equals(r) && !w.equals(s) && !w.equals(t) && !w.equals(u) && !w.equals(v)){ rijbewijstemp[i] = w; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
     }
@@ -103,15 +103,15 @@ public class Mens implements Comparable<Mens>{
     public Mens(String naam, Rijbewijs r, Rijbewijs s, Rijbewijs t, Rijbewijs u, Rijbewijs v, Rijbewijs w, Rijbewijs x) {
         int i = 0;
         this.naam = naam;
-       String[] rijbewijstemp = new String [7];
-         rijbewijstemp[0] = (r.toString());
+       Rijbewijs[] rijbewijstemp = new Rijbewijs [7];
+        rijbewijstemp[0] = r;
         i++;
-        if(!s.equals(r)){ rijbewijstemp[i] = (s.toString()); i++;}
-        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = (t.toString()); i++;}
-        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = (u.toString()); i++;}
-        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = (v.toString()); i++;}
-        if(!w.equals(r) && !w.equals(s) && !w.equals(t) && !w.equals(u) && !w.equals(v)){ rijbewijstemp[i] = (w.toString()); i++;}
-        if(!x.equals(r) && !x.equals(s) && !x.equals(t) && !x.equals(u) && !x.equals(v) && !x.equals(w)){ rijbewijstemp[i] = (x.toString()); i++;}
+        if(!s.equals(r)){ rijbewijstemp[i] = s; i++;}
+        if(!t.equals(r) && !t.equals(s)){ rijbewijstemp[i] = t; i++;}
+        if(!u.equals(r) && !u.equals(s) && !u.equals(t)){ rijbewijstemp[i] = u; i++;}
+        if(!v.equals(r) && !v.equals(s) && !v.equals(t) && !v.equals(u)){ rijbewijstemp[i] = v; i++;}
+        if(!w.equals(r) && !w.equals(s) && !w.equals(t) && !w.equals(u) && !w.equals(v)){ rijbewijstemp[i] = w; i++;}
+        if(!x.equals(r) && !x.equals(s) && !x.equals(t) && !x.equals(u) && !x.equals(v) && !x.equals(w)){ rijbewijstemp[i] = x; i++;}
         //rijbewijs = new String [i];
         rijbewijs = Arrays.copyOf(rijbewijstemp, i);
     }
@@ -127,7 +127,7 @@ public class Mens implements Comparable<Mens>{
         }
         else{
             String rrr = "";
-            for (String s: this.rijbewijs){
+            for (Rijbewijs s: this.rijbewijs){
                       rrr = rrr+s+", ";  
                     }
             rrr = rrr.substring(0, rrr.length()-2);
@@ -140,21 +140,23 @@ public class Mens implements Comparable<Mens>{
         return naam;
     }
 
-    public String[] getRijbewijs() {
+    public Rijbewijs[] getRijbewijs() {
         return rijbewijs;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.naam);
+        hash = 79 * hash + Arrays.deepHashCode(this.rijbewijs);
+        return hash;
+    }
+
     
     
     
 
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 89 * hash + Objects.hashCode(this.naam);
-        hash = 89 * hash + Objects.hashCode(this.rijbewijs);
-        return hash;
-    }
+    
 
     @Override
     public boolean equals(Object obj) {
@@ -176,8 +178,28 @@ public class Mens implements Comparable<Mens>{
 
     @Override
     public int compareTo(Mens o) {
-        return this.naam.compareTo(o.getNaam());
+              
+            return this.naam.compareTo(o.getNaam());
+    }     
+   
+    public boolean equals(Mens o){
+        
+        if (o == null) {
+            return false;
+        }
+               
+        if (!this.naam.equals(o.getNaam())) {
+            return false;
+        }
+
+        if (!Arrays.equals(this.rijbewijs, o.getRijbewijs())) {
+            return false;
+        }
+
+        return true;
+        
     }
     
+        
     
 }
